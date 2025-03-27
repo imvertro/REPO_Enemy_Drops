a100a6610af416ede8d2010e1ad09c5b83c855db
jeffb <jeffb@JEFFS-DESKTOP>
2025-03-27 18:19:09 -0400

    Add project files.

---
+++ REPO_Enemy_Drops/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPO_Enemy_Drops
+{
+    public class Class1
+    {
+    }
+}
