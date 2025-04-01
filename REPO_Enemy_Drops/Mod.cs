92da213bdc8a41a6c72a7f69491424896f09b1a4
ImVertro <j.s.bellmore@icloud.com>
2025-04-01 14:54:10 -0400

    Added Gun Tranq to spawn list

--- REPO_Enemy_Drops/Mod.cs
+++ REPO_Enemy_Drops/Mod.cs
@@ -20,19 +20,19 @@ namespace REPO_Enemy_Drops
             {
                 GameObject enable = enemy?.transform.Find("Enable")?.gameObject;
                 GameObject controller = enable?.transform.Find("Controller")?.gameObject;
-                List<string> upgrades = new List<string> { "Extra Jump", "Grab Range", "Energy", "Grab Strength", "Health", "Sprint Speed", "Tumble Launch" };
+                List<string> upgrades = new List<string> { "Upgrade Player Extra Jump", "Upgrade Player Grab Range", "Upgrade Player Energy", "Upgrade Player Grab Strength", "Upgrade Player Health", "Upgrade Player Sprint Speed", "Upgrade Player Tumble Launch", "Gun Tranq" };
                 string upgrade = upgrades[UnityEngine.Random.Range(0, upgrades.Count)];
                 // 10% chance to spawn a random upgrade if Gnome or Bang enemy
                 if (enemy.name.Contains("Gnome") || enemy.name.Contains("Bang"))
                 {
                     if (UnityEngine.Random.Range(0, 100) > 90)
                     {
-                        Repo_Lib.SpawnItem($"Item Upgrade Player {upgrade}", controller.transform.position + controller.transform.up * 2);
+                        Repo_Lib.SpawnItem($"Item {upgrade}", controller.transform.position + controller.transform.up * 2);
                     }
                 }
                 else
                 {
-                    Repo_Lib.SpawnItem($"Item Upgrade Player {upgrade}", controller.transform.position + controller.transform.up * 2);
+                    Repo_Lib.SpawnItem($"Item {upgrade}", controller.transform.position + controller.transform.up * 2);
                 }
             };
         }
